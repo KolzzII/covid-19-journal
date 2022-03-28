65605bf46f9ec40dc213625c8c3ab27f0f3ed146
Junior <junior.ng@outlook.com.br>
2022-03-28 15:41:58 -0300

    feat: válida id da notícia

--- app.py
+++ app.py
@@ -1,26 +1,14 @@
 from flask import Flask, render_template
-from noticia import Noticia
-
+from models import lista_noticias, lista_estados
 app = Flask(__name__)
-
-lista_noticias = [
-    Noticia(0, 'Covid-19', 'resumo', 'conteúdo'),
-    Noticia(1, 'Covid-19', 'resumo', 'conteúdo'),
-    Noticia(2, 'Covid-19', 'resumo', 'conteúdo'),
-    Noticia(3, 'Covid-19', 'resumo', 'conteúdo'),
-    Noticia(4, 'Covid-19', 'resumo', 'conteúdo'),
-    Noticia(5, 'Covid-19', 'resumo', 'conteúdo'),
-    Noticia(6, 'Covid-19', 'resumo', 'conteúdo'),
-    Noticia(7, 'Covid-19', 'resumo', 'conteúdo'),
-    Noticia(8, 'Covid-19', 'resumo', 'conteúdo'),
-    Noticia(9, 'Covid-19', 'resumo', 'conteúdo'),
-    Noticia(10, 'Covid-19', 'resumo', 'conteúdo')
-]
 
 @app.route("/")
 def home():
-    return render_template ("index.html", noticias=lista_noticias)
+    return render_template ("index.html", noticias=lista_noticias, estados=lista_estados)
 
 @app.route("/noticia/<id>")
 def exibir_noticia(id):
-    return render_template ("noticia.html", noticia=lista_noticias[int(id)])
+    for noticia in lista_noticias:
+        if noticia.get_id() == int(id):
+            return render_template("noticia.html", noticia=lista_noticias[int(id)])
+    return render_template ("index.html", noticias=lista_noticias)
