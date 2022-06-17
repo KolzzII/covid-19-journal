51f30c5074969d26f6b24ec2ad7beaf609ea7ce5
Junior <86266735+KolzzII@users.noreply.github.com>
2022-06-17 18:21:23 -0300

    feat: adiciona mensagem caso nao tenha comentarios

--- application/controller/estado_controller.py
+++ application/controller/estado_controller.py
@@ -5,7 +5,6 @@ from application.model.dao.estado_dao import EstadoDAO
 @app.route("/estado/<string:sigla>")
 def noticias_do_estado(sigla):
     list_estados = EstadoDAO().lista_estados()
-    print(list_estados)
     for estado in list_estados:
         if estado.get_sigla() == sigla:
             return render_template("estado.html", noticias=estado.get_noticia_lista(), estados=list_estados)
